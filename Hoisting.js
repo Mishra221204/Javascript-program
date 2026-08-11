1824a71a67bfd68b0fc3889a5f848a8af9c93ad3
Shubham Mishra <mishrashubham76395@gmail.com>
2026-08-11 04:44:27 -0400

    Add files via upload

---
+++ Hoisting.js
@@ -0,0 +1,46 @@
+//  Hoisting  in Java Script
+
+// code  run --> Execution context
+// 2 part divide 
+// 1 - Memory alloction
+// 2 - Exection Phase
+//   code run hone se pahle proces hota hai 
+
+// var a=10;
+// var b=20;
+// function addNumber(){
+    // var sum =a+b;
+
+    // console.log(sum);
+// }
+
+// addNumber();
+
+
+// function meet(num1,num2){
+    // const sum=num1+num2;
+    // 
+//    return sum;
+// }
+//  var math=meet(2,4);
+//  var math2=meet(5,7);
+//  console.log(math,math2);
+
+
+
+
+const Nested=(num3,num4)=> {
+    return num3+num4;
+
+}
+console.log(Nested(4,5))
+
+constn
+
+
+
+// Code kaise run hot hai
+// console.log(a);
+// let a=10;
+// let b=20;
+
